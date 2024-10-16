02ebb35e09e074def25dfca835842672cc2e42e4
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-10-16 16:08:04 +0200

    Library pour la séparation du jeux de donnée

--- Model_training.py
+++ Model_training.py
@@ -1,6 +1,8 @@
 import torch
 import tiatoolbox
 from tiatoolbox.models import PatchPredictor
-
+import os
+import random
+import shutil
 # Load pre-trained ResNet50 from TIAToolbox
 predictor = PatchPredictor(pretrained_model='resnet50-kather100k', batch_size=32)
